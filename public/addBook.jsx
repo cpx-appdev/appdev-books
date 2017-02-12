69dbde863cfa284567ef212a8e6a475def68cc00
Thomas <Thomas@DevBook>
2017-02-12 17:02:59 +0100

    Issue #2: added draft for reading isbn from image

--- public/addBook.jsx
+++ public/addBook.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import Quagga from "quagga";
+
 
 class AddBook extends React.Component {
     constructor(props) {
         super(props);
-
+        this.state = { scannedIsbn: "" };
         this.addBook = this.addBook.bind(this);
+        this.scanImage = this.scanImage.bind(this);
     }
 
     addBook() {
@@ -20,11 +23,41 @@ class AddBook extends React.Component {
         })
     }
 
+    scanImage(e) {
+        var file = e.target.files[0];
+        var reader = new FileReader();
+
+        reader.onload = () => {
+            Quagga.decodeSingle({
+                inputStream: {
+                    name: "Image",
+                    type: "ImageStream",
+                    src: reader.result
+                },
+                decoder: {
+                    readers: ["ean_reader"]
+                }
+            }, (result) => {
+                console.log(result.codeResult.code);
+                this.setState({ scannedIsbn: result.codeResult.code });
+            });
+        };
+
+        if (file) {
+            reader.readAsDataURL(file);
+        }
+    }
+
+
+
     render() {
         return <div>
             <input type="text" ref={(input) => this.inputIsbn = input} />
             <input type="text" ref={(input) => this.inputTitle = input} />
             <button onClick={this.addBook}>Add Book</button>
+            <br />
+            <input type="text" value={this.state.scannedIsbn} />
+            <input onChange={this.scanImage} type="file" capture="camera" accept="image/*" />
         </div>;
     }
 }
